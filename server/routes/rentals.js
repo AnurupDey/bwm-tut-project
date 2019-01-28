f9f9d9fd5606e5abaa1951cc4b4222ea6f646368
Anurup Dey <anu.rup.dey98@gmail.com>
2019-01-28 15:35:12 +0530

    Adds basic server setup, adds rental routes, adds DB mongo connection

---
+++ server/routes/rentals.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+const Rental = require("../models/rental");
+
+router.get('',function(req,res){
+    Rental.find({},(err,foundRentals)=>{
+        res.json(foundRentals);
+    });
+});
+
+router.get('/:id',(req,res)=>{
+    const rentalId = req.params.id;
+    Rental.findById(rentalId,(err,foundRental)=>{
+        if(err){
+            res.status(422).send({
+                errors: [
+                    {
+                        title: 'Rental Error',
+                        details: 'Could not find Rentals!'
+                    }
+                ]
+            })
+        }
+        res.json(foundRental);
+    });
+});
+
+module.exports = router;
